908bc5a62bd4e3a9b3e65c36150fb5ac60395ea1
RickBragaThePiEAD <202402956231@alunos.estacio.br>
2025-05-19 14:42:32 +0000

    Time to code

---
+++ hora_de_codificar.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    // Usando o for para calcular a tabuada de um número
+    int num;
+    printf("Digite o núero que você quer ver a sua tabuada: ");
+    scanf("%d", &num);
+    printf("Tabuada do %d\n", num);
+    for (int i = 0; i <= 10; i++){
+        printf("%d x %d = %d\n", num, i, num * i);
+    }
+    printf("\n");
+    
+    // Usando o while e o if para imprimir os números ímpares entre 0 e 10
+    int i = 0;
+    while (i <= 10){
+        if (i % 2 != 0){
+            printf("%d\n", i);
+        }
+        i++;
+    }
+   
+    // Digitando um número par para sair do programa
+    int numero;
+    do {
+        printf("Digite um número par para sair do programa: ");
+        scanf("%d", &numero);
+        if (numero % 2 == 0){
+        printf("%d é par!\n", numero);
+        } else {
+        printf("%d não é impar!\n", numero);
+        }
+    } while (numero % 2 != 0);
+
+    printf("Você digitou um número par, o programa vai sair!\n");
+    
+    return 0;
+}
